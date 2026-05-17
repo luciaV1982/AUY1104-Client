8005b75192f5be9b777ae9b0e89e81ae4a8482d2
Amparo Lucia Villalobos Ospina <226887593+luciaV1982@users.noreply.github.com>
2026-05-16 22:34:56 -0400

    Update test message for API functionality

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 
-const mensaje = "API de Lucia funcionando";
-assert.strictEqual(mensaje, "API de Lucia funcionando");
+const mensaje = "API Prueba 2 funcionando en Kubernetes";
+assert.strictEqual(mensaje, "API Prueba 2 funcionando en Kubernetes");
 
 console.log("Test ejecutado correctamente");
