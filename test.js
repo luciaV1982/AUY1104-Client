a9c1c059be8fafe49f6ef5361f46a11838232f64
Amparo Lucía Villalobos Ospina <226887593+luciaV1982@users.noreply.github.com>
2026-06-13 21:42:10 -0400

    lab3.1: introducir falla en test

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 
-const mensaje = "API Prueba 2 funcionando en Kubernetes";
+const mensaje = "mi-api";
 assert.strictEqual(mensaje, "API Prueba 2 funcionando en Kubernetes");
 
 console.log("Test ejecutado correctamente");
