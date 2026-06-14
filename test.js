4c4c7f1c51512e1ea268fec23b60bb7570e0a732
Amparo Lucía Villalobos Ospina <226887593+luciaV1982@users.noreply.github.com>
2026-06-13 22:11:28 -0400

    lab3.1: restaurar valor correcto

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 const assert = require("assert");
 
-const mensaje = "mi-api";
-assert.strictEqual(mensaje, "API Prueba 2 funcionando en Kubernetes");
+const mensaje = "API Prueba 2 funcionando en Kubernetes";
+
+assert.strictEqual(
+  mensaje,
+  "API Prueba 2 funcionando en Kubernetes"
+);
 
 console.log("Test ejecutado correctamente");
