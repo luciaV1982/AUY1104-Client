95a1ced43e582f1c4edbd70d8f368e9fba7572e9
Amparo Lucia Villalobos Ospina <226887593+luciaV1982@users.noreply.github.com>
2026-06-26 20:44:51 -0400

    Update message to 'API Prueba 3 funcionando en Kubernetes'

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 
-const mensaje = "API Prueba 2 funcionando en Kubernetes";
+const mensaje = "API Prueba 3 funcionando en Kubernetes";
 
 assert.strictEqual(
   mensaje,
