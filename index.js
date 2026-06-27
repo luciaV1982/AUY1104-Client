8f4f19572ae8b3684e653fac299968320c9ac914
Amparo Lucía Villalobos Ospina <226887593+luciaV1982@users.noreply.github.com>
2026-06-26 20:24:20 -0400

    EA3: actualizar mensajes a Prueba 3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-  res.end("API Prueba 2 funcionando en Kubernetes");
+  res.end("API Prueba 3 funcionando en Kubernetes");
 });
 
 server.listen(PORT, "0.0.0.0", () => {
